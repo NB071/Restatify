95df2290c72944a3631b7a62ebfd279354f248dd
Nima Bargestan <nimam700@gmail.com>
2025-05-06 17:26:55 -0400

    client: Develop CTA section

---
+++ client/src/app/(nondashboard)/landing/FooterSection.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import {
+	FaFacebookF,
+	FaInstagram,
+	FaTwitter,
+	FaLinkedinIn,
+	FaYoutube,
+} from "react-icons/fa";
+
+const FooterSection = () => {
+	return <footer>FooterSection</footer>;
+};
+
+export default FooterSection;
