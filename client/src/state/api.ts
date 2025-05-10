54d4b7db004f4bb7c251f170ca161d61322d7c45
Nima Bargestan <nimam700@gmail.com>
2025-05-09 23:01:02 -0400

    client/src/state/api: add configurations to redux api

--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -1,12 +1,53 @@
+import { Manager, Tenant } from "@/types/prismaTypes";
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { fetchAuthSession, getCurrentUser } from "aws-amplify/auth";
 
 export const api = createApi({
-  baseQuery: fetchBaseQuery({
-    baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL,
-  }),
-  reducerPath: "api",
-  tagTypes: [],
-  endpoints: (build) => ({}),
+	baseQuery: fetchBaseQuery({
+		baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL,
+		prepareHeaders: async (headers) => {
+			const session = await fetchAuthSession();
+			const idToken = session?.tokens ?? {};
+
+			if (idToken) {
+				headers.set("Authorization", `Bearer ${idToken}`);
+			}
+			return headers;
+		},
+	}),
+	reducerPath: "api",
+	tagTypes: [],
+	endpoints: (build) => ({
+		getAuthUser: build.query<User, void>({
+			queryFn: async (_, _queryApi, _extraOpt, fetchBaseQ) => {
+				try {
+					const session = await fetchAuthSession();
+					const user = await getCurrentUser();
+					const { idToken } = session.tokens ?? {};
+					const userRole = idToken?.payload["custom:role"] as string;
+
+					const endpoint =
+						userRole === "manager"
+							? `/managers/${user.userId}`
+							: `/tenants/${user.userId}`;
+
+					let userDetailsRes = await fetchBaseQ(endpoint);
+
+					// TODO: not user exists
+
+					return {
+						data: {
+							cognitoInfo: { ...user },
+							userInfo: userDetailsRes.data as Tenant | Manager,
+							userRole,
+						},
+					};
+				} catch (e: any) {
+					return { error: e.message || "Error fetching user data" };
+				}
+			},
+		}),
+	}),
 });
 
 export const {} = api;
