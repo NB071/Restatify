60a9021fbb427ea28d679446c547eff7000cf870
Nima Bargestan <nimam700@gmail.com>
2025-05-09 23:33:46 -0400

    server/src: build 'get' and 'create' manager routes/controllers

---
+++ server/src/controllers/managerControllers.ts
@@ -0,0 +1,52 @@
+import { Request, Response } from "express";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export const getManager = async (
+	res: Response,
+	req: Request
+): Promise<void> => {
+	try {
+		const { cognitoId } = req.params;
+		const manager = await prisma.manager.findUnique({
+			where: {
+				cognitoId,
+			},
+		});
+
+		if (manager) {
+			res.status(200).json(manager);
+		} else {
+			res.status(404).json({ message: "Manager not found" });
+		}
+	} catch (error: any) {
+		res.status(500).json({
+			message: `error retrieving manager: ${error.message}`,
+		});
+	}
+};
+
+export const createManager = async (
+	req: Request,
+	res: Response
+): Promise<void> => {
+	try {
+		const { cognitoId, name, email, phoneNumber } = req.body;
+
+		const newManager = await prisma.manager.create({
+			data: {
+				cognitoId,
+				name,
+				email,
+				phoneNumber,
+			},
+		});
+
+		res.status(201).json(newManager);
+	} catch (error: any) {
+		res.status(500).json({
+			message: `error creating manager: ${error.message}`,
+		});
+	}
+};
