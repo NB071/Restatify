9ebdc14c21c47b815349f77824cb94a9f3b79a68
Nima Bargestan <nimam700@gmail.com>
2025-05-11 11:53:12 -0400

    client: implement safe routing on layouts

--- client/src/app/(dashboard)/layout.tsx
+++ client/src/app/(dashboard)/layout.tsx
@@ -4,11 +4,42 @@ import Navbar from "@/components/Navbar";
 import { SidebarProvider } from "@/components/ui/sidebar";
 import Sidebar from "@/components/AppSidebar";
 import { NAVBAR_HEIGHT } from "@/lib/constants";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useGetAuthUserQuery } from "@/state/api";
+import { usePathname, useRouter } from "next/navigation";
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
-	const { data: authUser } = useGetAuthUserQuery();
+	const { data: authUser, isLoading: isAuthLoading } = useGetAuthUserQuery();
+	const router = useRouter();
+	const pathname = usePathname();
+	const [isLoading, setIsLoading] = useState(true);
+
+	useEffect(() => {
+		if (authUser) {
+			const userRole = authUser.userRole?.toLowerCase();
+			if (
+				(userRole === "manager" && pathname.startsWith("/tenants")) ||
+				(userRole === "tenant" && pathname.startsWith("/managers"))
+			) {
+				router.push(
+					userRole === "manager"
+						? "/managers/properties"
+						: "/tenants/favorites",
+					{ scroll: false }
+				);
+			} else {
+				setIsLoading(false);
+			}
+		}
+	}, [authUser, pathname, router]);
+
+	if (isAuthLoading || isLoading) {
+		return (
+			<div className="flex items-center justify-center min-h-screen">
+				<div className="loader"> Loading...</div>
+			</div>
+		);
+	}
 
 	if (!authUser?.userRole) {
 		return null;
--- client/src/app/(nondashboard)/layout.tsx
+++ client/src/app/(nondashboard)/layout.tsx
@@ -2,10 +2,36 @@
 
 import Navbar from "@/components/Navbar";
 import { useGetAuthUserQuery } from "@/state/api";
-import React from "react";
+import { usePathname, useRouter } from "next/navigation";
+import React, { useEffect, useState } from "react";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
-	const { data: authUser } = useGetAuthUserQuery();
+	const { data: authUser, isLoading: isAuthLoading } = useGetAuthUserQuery();
+	const router = useRouter();
+	const pathname = usePathname();
+	const [isLoading, setIsLoading] = useState(true);
+
+	useEffect(() => {
+		if (authUser) {
+			const userRole = authUser.userRole?.toLowerCase();
+			if (
+				(userRole === "manager" && pathname.startsWith("/search")) ||
+				(userRole === "manager" && pathname === "/")
+			) {
+				router.push("/managers/properties", { scroll: false });
+			} else {
+				setIsLoading(false);
+			}
+		}
+	}, [authUser, pathname, router]);
+
+	if (isAuthLoading || isLoading) {
+		return (
+			<div className="flex items-center justify-center min-h-screen">
+				<div className="loader"> Loading...</div>
+			</div>
+		);
+	}
 	return (
 		<>
 			<Navbar />
