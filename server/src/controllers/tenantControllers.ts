70a60f99b9933dae9d2c197ac42d535596327a3c
Nima Bargestan <nimam700@gmail.com>
2025-05-12 21:40:01 -0400

    server: build getCurrentResidences endpoint for tenants

--- server/src/controllers/tenantControllers.ts
+++ server/src/controllers/tenantControllers.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import { PrismaClient } from "@prisma/client";
 import { HTTP_STATUS, MESSAGES } from "../constants";
+import { wktToGeoJSON } from "@terraformer/wkt";
 
 const prisma = new PrismaClient();
 
@@ -83,6 +84,71 @@ export const updateTenant = async (
 	} catch (error: any) {
 		res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({
 			message: MESSAGES.TENANT.ERR_UPDATING_TENANT.concat(
+				" : ",
+				error.message
+			),
+		});
+	}
+};
+
+export const getCurrentResidences = async (
+	req: Request,
+	res: Response
+): Promise<void> => {
+	try {
+		const { cognitoId } = req.params;
+		const tenant = await prisma.tenant.findUnique({
+			where: {
+				cognitoId,
+			},
+		});
+
+		if (!tenant) {
+			res.status(HTTP_STATUS.NOT_FOUND).json({
+				message: MESSAGES.TENANT.TENANT_NOT_FOUND,
+			});
+			return;
+		}
+
+		const properties = await prisma.property.findMany({
+			where: {
+				tenants: { some: { cognitoId } },
+			},
+			include: { location: true },
+		});
+
+		const residencesWithLocation = await Promise.all(
+			properties.map(async (property) => {
+				const coordinates: { coordinates: string }[] =
+					await prisma.$queryRaw`
+					SELECT ST_asText(coordinates) AS coordinates 
+					FROM "Location"
+					WHERE id = ${property.location.id}
+				`;
+
+				const geoJSON: any = wktToGeoJSON(
+					coordinates[0]?.coordinates || ""
+				);
+				const longitude = geoJSON.coordinates[0];
+				const latitude = geoJSON.coordinates[1];
+
+				return {
+					...property,
+					location: {
+						...property.location,
+						coordinates: {
+							longitude,
+							latitude,
+						},
+					},
+				};
+			})
+		);
+
+		res.status(HTTP_STATUS.OK).json(residencesWithLocation);
+	} catch (error: any) {
+		res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({
+			message: MESSAGES.TENANT.ERR_RETRIEVING_TENANT_RESIDENCES.concat(
 				" : ",
 				error.message
 			),
--- server/src/routes/tenantRoutes.ts
+++ server/src/routes/tenantRoutes.ts
@@ -3,11 +3,13 @@ import {
 	getTenant,
 	createTenant,
 	updateTenant,
+	getCurrentResidences,
 } from "../controllers/tenantControllers";
 
 const router = express.Router();
 
 router.route("/:cognitoId").get(getTenant).put(updateTenant);
 router.post("/", createTenant);
+router.get("/:cognitoId/currect-residences", getCurrentResidences);
 
 export default router;
